9eb9a3d4f3e30a94eb1aa70e92bb84080f79c242
italoalvees <italo_alvees@live.com>
2019-08-30 16:15:18 -0300

    Regressão +- feita

--- Regressão/Regressão.py
+++ Regressão/Regressão.py
@@ -2,30 +2,89 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.preprocessing import Normalizer
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import linear_model
+from sklearn.metrics import r2_score , mean_absolute_error
+from sklearn.neighbors import KNeighborsRegressor
+
+def normalizar(coluna):
+	max = coluna.max()
+	min = coluna.min()
+	divisor = max 
+
+	for i in range(len(coluna)):
+		coluna.at[i] = coluna.at[i]/divisor
+
+	return coluna
+
+
 
 rg = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+testY = pd.read_csv('sample_submission.csv')
+
+testY = testY['SalePrice']
 
 rg = rg.drop(columns='Id')
+testX = test.drop(columns='Id')
+
+rgX = rg.drop(columns='SalePrice')
+rgY = rg['SalePrice']
+
+rgX['PoolQC'] = rgX['PoolQC'].fillna(0)
+rgX['GarageYrBlt'] = rgX['GarageYrBlt'].interpolate(method= 'pad')
+rgX['LotFrontage'] = rgX['LotFrontage'].fillna(0)
+
+rgX = rgX.fillna('NaN')
+
+testX['PoolQC'] = testX['PoolQC'].fillna(0)
+testX['GarageYrBlt'] = testX['GarageYrBlt'].interpolate(method= 'pad')
+testX['LotFrontage'] = testX['LotFrontage'].fillna(0)
+
+testX = testX.fillna('NaN')
+
+le = preprocessing.LabelEncoder()
+
+for i in rgX.columns:
+	rgX[i] = le.fit_transform(rgX[i].astype(str))
+	testX[i] = le.fit_transform(testX[i].astype(str))
+
+rgX = Normalizer().fit_transform(rgX)
+testX = Normalizer().fit_transform(testX)
+# rgY = normalizar(rgY)
+# testY = normalizar(testY)
 
 
-for i in range(10):
-	print(i)
+#_____________________________________________________________________________
 
-for i in rg.columns:
-	if(type(rg[i]) == "<class 'str'>"):
-		rg[i] = rg[i].fillna("nan")
-	else:
-		rg[i] = rg[i].fillna(0)
-	print(rg[i])
+Bayes = linear_model.BayesianRidge()
 
+Bayes.fit(rgX , rgY)
 
+BayesPredict = Bayes.predict(testX)
 
-for i in rg.columns:
-	le = preprocessing.LabelEncoder()
-	rg[i] = le.fit_transform(rg[i])
+print("Bayes Score:" , r2_score(testY, BayesPredict), mean_absolute_error(testY, BayesPredict))
 
-print(rg)
+#_____________________________________________________________________________
 
-rg = Normalizer().fit_transform(rg)
+Linear = linear_model.LinearRegression()
 
-print(rg)
+Linear.fit(rgX , rgY)
+
+LinearPredict = Linear.predict(testX)
+
+print("Linear Score:", r2_score(testY, LinearPredict), mean_absolute_error(testY, LinearPredict))
+
+#______________________________________________________________________________
+
+Neighbors = KNeighborsRegressor()
+
+Neighbors.fit(rgX , rgY)
+
+NeighborsPredict = Neighbors.predict(testX)
+
+print("KNeighbors Score:", r2_score(testY, NeighborsPredict) , mean_absolute_error(testY, NeighborsPredict))
+
+#______________________________________________________________________________
+
+for i in range(len(testY)):
+	print(i ,testY[i] , NeighborsPredict[i])
